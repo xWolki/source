ef54e5856427904b93616ac41da9c8db2192b2c3
xWolki <alessandro_away2@hotmail.com>
2016-03-09 12:15:12 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -309,9 +309,9 @@
             songstats: true,
             commandLiteral: "!",
             blacklists: {
-                NSFW: "https://rawgit.com/bscBot/custom/master/blacklists/NSFWlist.json",
-                OP: "https://rawgit.com/bscBot/custom/master/blacklists/OPlist.json",
-                BANNED: "https://rawgit.com/bscBot/custom/master/blacklists/BANNEDlist.json"
+                NSFW: "https://rawgit.com/xWolki/custom/master/blacklists/NSFWlist.json",
+                OP: "https://rawgit.com/xWolki/custom/master/blacklists/OPlist.json",
+                BANNED: "https://rawgit.com/xWolki/custom/master/blacklists/BANNEDlist.json"
             }
         },
         room: {
@@ -2476,7 +2476,7 @@
                         if (msg.length <= cmd.length + 1) return API.sendChat(subChat(basicBot.chat.currentlang, {language: basicBot.settings.language}));
                         var argument = msg.substring(cmd.length + 1);
 
-                        $.get("https://rawgit.com/bscBot/source/master/lang/langIndex.json", function (json) {
+                        $.get("https://rawgit.com/xWolki/source/master/lang/langIndex.json", function (json) {
                             var langIndex = json;
                             var link = langIndex[argument.toLowerCase()];
                             if (typeof link === "undefined") {
