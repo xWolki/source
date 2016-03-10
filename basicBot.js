fa3a0e0e8a00b081e227ad6edd4b9c2cefeb2387
xWolki <alessandro_away2@hotmail.com>
2016-03-10 09:09:51 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1953,13 +1953,13 @@
                 }
             },
 
-            amzCommand: {
-                command: 'amz',
+            trollarCommand: {
+                command: 'trollar',
                 rank: 'user',
                 type: 'startsWith',
-                getAmz: function (chat) {
-                    var c = Math.floor(Math.random() * basicBot.chat.amzs.length);
-                    return basicBot.chat.amzs[c];
+                getTrollar: function (chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.trolls.length);
+                    return basicBot.chat.trolls[c];
                 },
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
@@ -1969,20 +1969,20 @@
 
                         var space = msg.indexOf(' ');
                         if (space === -1) {
-                            API.sendChat(basicBot.chat.eatamz);
+                            API.sendChat(basicBot.chat.eattrollar);
                             return false;
                         }
                         else {
                             var name = msg.substring(space + 2);
                             var user = basicBot.userUtilities.lookupUserName(name);
                             if (user === false || !user.inRoom) {
-                                return API.sendChat(subChat(basicBot.chat.nouseramz, {name: name}));
+                                return API.sendChat(subChat(basicBot.chat.nousertrollar, {name: name}));
                             }
                             else if (user.username === chat.un) {
-                                return API.sendChat(subChat(basicBot.chat.selfamz, {name: name}));
+                                return API.sendChat(subChat(basicBot.chat.selftrollar, {name: name}));
                             }
                             else {
-                                return API.sendChat(subChat(basicBot.chat.amz, {nameto: user.username, namefrom: chat.un, amz: this.getAmz()}));
+                                return API.sendChat(subChat(basicBot.chat.trollar, {nameto: user.username, namefrom: chat.un, trollar: this.getTrollar()}));
                             }
                         }
                     }
